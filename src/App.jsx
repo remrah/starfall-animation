64b078cabd77a92ab56fada163941938c5a65cd2
remrah <emrahgokkaya@gmail.com>
2026-04-12 14:57:17 +0300

    Fix: syntax and button click

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <>
-      {showWarp && <WarpAnimation onClose={handleClose} />
+      {showWarp && <WarpAnimation onClose={handleClose} />}
       {!showWarp && (
         <div style={{
           display: 'flex',
